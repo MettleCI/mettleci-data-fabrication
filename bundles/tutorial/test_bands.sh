045ec12555b928d1ab5cc8e605fa381862604f31
John McKeever <john.mckeever@datamigrators.com>
2025-07-16 15:11:18 +1000

    Added bands test script

--- bundles/tutorial/test_bands.sh
+++ bundles/tutorial/test_bands.sh
@@ -4,22 +4,22 @@
 # This script runs tests for the bands tutorial bundle using the mettleci tool.
 shopt -s xpg_echo
 
-export BUNDLE_FILE=./bands.json
+export bundle=./bands.json
 export bands=("beatles" "stones" "queen")
 
 echo "\nListing available generators"
-mettleci fabrication list -path $BUNDLE_FILE
+mettleci fabrication list -path $bundle -include-params
 
 echo "\nRunning band.member"
-mettleci fabrication test -path $BUNDLE_FILE -generator band.member
+mettleci fabrication test -path $bundle -generator band.member
 
 echo "\nRunning band.album"
-mettleci fabrication test -path $BUNDLE_FILE -generator band.album
+mettleci fabrication test -path $bundle -generator band.album
 
 for band in "${bands[@]}"; do
     echo "\nRunning band.member for $band"
-    mettleci fabrication test -path $BUNDLE_FILE -generator band.member
+    mettleci fabrication test -path $bundle -generator band.member
 
     echo "\nRunning band.album for $band"
-    mettleci fabrication test -path $BUNDLE_FILE -generator band.album
+    mettleci fabrication test -path $bundle -generator band.album
 done
